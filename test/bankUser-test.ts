6b43274be78d73aa89da9690980bfdc717b0d98c
keyminy <imanaifs@gmail.com>
2023-08-21 20:53:16 +0900

    task2, Num sequence contract

---
+++ test/bankUser-test.ts
@@ -0,0 +1,89 @@
+import { Contract, fromNano, Signer, toNano } from "locklift";
+import { FactorySource } from "../build/factorySource";
+import { expect } from "chai";
+
+let bank: Contract<FactorySource["bank"]>;
+let user: Contract<FactorySource["user"]>;
+
+let signer1: Signer;
+
+describe("BankUserTest", async function () {
+  before(async () => {
+    signer1 = (await locklift.keystore.getSigner("0"))!;
+  });
+  describe("Contracts", async function () {
+    it("Deploy Bank and User", async function () {
+      bank = await locklift.factory
+        .deployContract({
+          contract: "bank",
+          initParams: {},
+          constructorParams: {
+            _interestRate: 5,
+            // _owner: bank.address,
+          },
+          value: toNano(10),
+          publicKey: signer1.publicKey,
+        })
+        .then(res => res.contract);
+
+      user = await locklift.factory
+        .deployContract({
+          contract: "user",
+          initParams: {},
+          constructorParams: {
+            _bank: bank.address,
+            _initialBalance: 222,
+          },
+          value: toNano(10),
+          publicKey: signer1.publicKey,
+        })
+        .then(res => res.contract);
+    });
+
+    it("Interact with contract", async function () {
+      const { traceTree } = await locklift.tracing.trace(
+        user.methods
+          .borrowMoney({
+            _amount: 1000,
+          })
+          .sendExternal({
+            publicKey: signer1.publicKey,
+          }),
+      );
+      await traceTree?.beautyPrint();
+      const totalrepayAmount = await bank.methods.calulating().call();
+      // Number(totalrepayAmount.value0
+      {
+        const { traceTree } = await locklift.tracing.trace(
+          user.methods.repayLoan({ _repayAmount: 1030 }).sendExternal({
+            publicKey: signer1.publicKey,
+          }),
+        );
+        await traceTree?.beautyPrint();
+      }
+
+      const response = await bank.methods.getProfit({}).call();
+      console.log(response);
+      const response2 = await user.methods.getMoney().call();
+      console.log(response2);
+      // const { traceTree: SecondLoan } = await locklift.tracing.trace(
+      //   user.methods
+      //     .borrowMoney({
+      //       _amount: 100000,
+      //     })
+      //     .sendExternal({
+      //       publicKey: signer1.publicKey,
+      //     }),
+      // );
+      // await SecondLoan?.beautyPrint();
+      // const { traceTree: SecondRepaid } = await locklift.tracing.trace(
+      //   user.methods.repayLoan({}).sendExternal({
+      //     publicKey: signer1.publicKey,
+      //   }),
+      // );
+      // await SecondRepaid?.beautyPrint();
+      // const response2 = await bank.methods.getProfit({}).call();
+      // console.log(response2);
+    });
+  });
+});
---
+++ test/numSequence.ts
@@ -0,0 +1,54 @@
+import { expect } from "chai";
+import { Contract, Signer, toNano, WalletTypes } from "locklift";
+import { FactorySource } from "../build/factorySource";
+import { Account } from "everscale-standalone-client";
+
+let sample: Contract<FactorySource["numSequence"]>;
+let signer: Signer;
+let user: Account;
+describe("Test Sample contract", async function () {
+  before(async () => {
+   //signer : 계약을 실행하는 사람
+    signer = (await locklift.keystore.getSigner("0"))!;
+  });
+  describe("Contracts", async function () {
+    it("Deploy contract", async function () {
+      const INIT_STATE = 0;
+      const { contract } = await locklift.factory.deployContract({
+        contract: "numSequence",
+        publicKey: signer.publicKey,
+        initParams: {
+        },
+        constructorParams: {
+            len : 3
+        },
+        value: locklift.utils.toNano(2),
+      });
+      sample = contract;
+      user = await locklift.factory.accounts
+        .addNewAccount({
+          type: WalletTypes.EverWallet,
+          value: toNano(10),
+          publicKey: signer.publicKey,
+        })
+        .then(res => res.account);
+      expect(await locklift.provider.getBalance(sample.address).then(balance => Number(balance))).to.be.above(0);
+    });
+
+    it("Interact with contract", async function () {
+      // destructing으로 표현가능
+        let {arr} = await sample.methods.arr().call();
+        console.log(arr); //[ '10', '233', '5' ]
+        const sumArray = arr.map(Number)
+        .reduce((a,b) => a+b,0);
+        console.log(sumArray); //248
+        await sample.methods.check({inputNum : 275}).sendExternal({publicKey:signer.publicKey});
+        const {sum} = await sample.methods.sum().call();
+         const {arr: newArr} = await sample.methods.arr().call();
+        console.log(newArr); //[ '10', '233', '5' ]
+        console.log('sum : ',sum);
+        expect(Number(sum)).to.be.equal(sumArray);
+        
+    });
+  });
+});

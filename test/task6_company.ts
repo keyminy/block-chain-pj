701f2836bb3f2fa80034ea5c7531c8aaa821d3f8
keyminy <imanaifs@gmail.com>
2023-09-26 15:26:24 +0900

    locklift test of task6

--- test/task6_company.ts
+++ test/task6_company.ts
@@ -1,89 +1,155 @@
-import { Contract, fromNano, Signer, toNano } from "locklift";
+import { Contract, Signer, toNano, WalletTypes } from "locklift";
 import { FactorySource } from "../build/factorySource";
-import { expect } from "chai";
+import { Account } from "everscale-standalone-client";
 
-let bank: Contract<FactorySource["bank"]>;
-let user: Contract<FactorySource["User"]>;
+let sample: Contract<FactorySource["Fibonacci"]>;
 
-let signer1: Signer;
+type Company = {
+  signer: Signer;
+  ownerAccount: Account;
+  contract: Contract<FactorySource["Company"]>;
+  workers: Array<Worker>;
+};
 
-describe("BankUserTest", async function () {
-  before(async () => {
-    signer1 = (await locklift.keystore.getSigner("0"))!;
-  });
+type Worker = {
+  workerAccount: Account;
+  workerContract: Contract<FactorySource["Worker"]>;
+};
+
+const companies: Array<Company> = [];
+
+describe("Test Sample contract", async function () {
   describe("Contracts", async function () {
-    it("Deploy Bank and User", async function () {
-      bank = await locklift.factory
-        .deployContract({
-          contract: "bank",
-          initParams: {},
-          constructorParams: {
-            _interestRate: 5,
-            _owner: `0x${signer1.publicKey}`,
-          },
-          value: toNano(10),
-          publicKey: signer1.publicKey,
-        })
-        .then(res => res.contract);
+    it("Deploy contract", async function () {
+      //create companies
+      for (let companyIdx of [1, 2]) {
+        const signer = (await locklift.keystore.getSigner(`${companyIdx}`))!;
+        const companyOwnerAccount = await locklift.factory.accounts
+          .addNewAccount({
+            type: WalletTypes.EverWallet,
+            value: toNano(10),
+            publicKey: signer.publicKey,
+            nonce: locklift.utils.getRandomNonce(),
+          })
+          .then(res => res.account);
+        console.log(
+          `Company ${companyIdx} Owner account: ${companyOwnerAccount.address}`,
+        );
+        const { code: workerCode } =
+          locklift.factory.getContractArtifacts("Worker");
+        const { contract: companyContract } =
+          await locklift.factory.deployContract({
+            contract: "Company",
+            publicKey: signer.publicKey,
+            initParams: {
+              owner: companyOwnerAccount.address,
+              workerCode,
+            },
+            constructorParams: {},
+            value: toNano(1),
+          });
+        console.log(
+          `Company ${companyIdx} address: ${companyContract.address}`,
+        );
 
-      user = await locklift.factory
-        .deployContract({
-          contract: "User",
-          initParams: {},
-          constructorParams: {
-            _bank: bank.address,
-            _initialBalance: 222,
-          },
-          value: toNano(10),
-          publicKey: signer1.publicKey,
-        })
-        .then(res => res.contract);
+        let workers: Array<Worker> = [];
+        // create workers
+        for (let workerIdx of [1, 2]) {
+          let { account: workerOwnerAccount } =
+            await locklift.factory.accounts.addNewAccount({
+              type: WalletTypes.EverWallet,
+              value: toNano(10),
+              publicKey: signer.publicKey,
+              nonce: locklift.utils.getRandomNonce(),
+            });
+          console.log(
+            `Company ${companyIdx} Worker ${workerIdx} account: ${workerOwnerAccount.address}`,
+          );
+
+          const { contract: workerContract } =
+            await locklift.factory.deployContract({
+              contract: "Worker",
+              constructorParams: {},
+              initParams: {
+                owner: workerOwnerAccount.address,
+                company: companyContract.address,
+              },
+              value: toNano(1),
+              publicKey: signer.publicKey,
+            });
+          console.log(
+            `Company ${companyIdx} Worker ${workerIdx} contract: ${workerContract.address}`,
+          );
+
+          workers.push({
+            workerAccount: workerOwnerAccount,
+            workerContract,
+          });
+        }
+
+        const company: Company = {
+          signer,
+          ownerAccount: companyOwnerAccount,
+          workers,
+          contract: companyContract,
+        };
+        companies.push(company);
+      }
     });
 
-    it("Interact with contract", async function () {
-      const { traceTree } = await locklift.tracing.trace(
-        user.methods
-          .borrowMoney({
-            _amount: 1000,
-          })
-          .sendExternal({
-            publicKey: signer1.publicKey,
-          }),
-      );
-      await traceTree?.beautyPrint();
-      const totalrepayAmount = await bank.methods.calulating().call();
-      // Number(totalrepayAmount.value0
-      {
-        const { traceTree } = await locklift.tracing.trace(
-          user.methods.repayLoan({ _repayAmount: 1030 }).sendExternal({
-            publicKey: signer1.publicKey,
-          }),
-        );
-        await traceTree?.beautyPrint();
+    it("Register workers", async function () {
+      for (let company of companies) {
+        for (let worker of company.workers) {
+          const { traceTree } = await locklift.tracing.trace(
+            company.contract.methods
+              .registerWorker({
+                _worker: worker.workerContract.address,
+              })
+              .send({
+                from: company.ownerAccount.address,
+                amount: toNano(1),
+              }),
+          );
+          await traceTree?.beautyPrint();
+        }
       }
-
-      const response = await bank.methods.getProfit({}).call();
-      console.log(response);
-      const response2 = await user.methods.getMoney().call();
-      console.log(response2);
-      // const { traceTree: SecondLoan } = await locklift.tracing.trace(
-      //   user.methods
-      //     .borrowMoney({
-      //       _amount: 100000,
-      //     })
-      //     .sendExternal({
-      //       publicKey: signer1.publicKey,
-      //     }),
-      // );
-      // await SecondLoan?.beautyPrint();
-      // const { traceTree: SecondRepaid } = await locklift.tracing.trace(
-      //   user.methods.repayLoan({}).sendExternal({
-      //     publicKey: signer1.publicKey,
-      //   }),
-      // );
-      // await SecondRepaid?.beautyPrint();
-      // const response2 = await bank.methods.getProfit({}).call();
-      // console.log(response2);
+    });
+    it("Do work", async function () {
+      for (let company of companies) {
+        for (let worker of company.workers) {
+          const { traceTree } = await locklift.tracing.trace(
+            worker.workerContract.methods.startWork().send({
+              from: worker.workerAccount.address,
+              amount: toNano(1),
+            }),
+          );
+          await traceTree?.beautyPrint();
+        }
+      }
+    });
+    it("Check details", async function () {
+      for (let company of companies) {
+        const companyDetails = await company.contract.methods
+          .getDetails()
+          .call();
+        console.log(
+          `Company ${company.contract.address} details: ${JSON.stringify(
+            companyDetails,
+            null,
+            4,
+          )}`,
+        );
+        for (let worker of company.workers) {
+          const details = await worker.workerContract.methods
+            .getDetails()
+            .call();
+          console.log(
+            `Company ${company.contract.address} Worker ${
+              worker.workerContract.address
+            } details: ${JSON.stringify(details, null, 4)}`,
+          );
+        }
+      }
     });
   });
 });
